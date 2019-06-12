649f7288adbe8453aef08b6b3ecce85268bcb582
ddifa86 <trkim@vms-solutions.com>
2019-06-12 19:59:40 +0900

    asdf

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -8,7 +8,7 @@ namespace GitHubTest
         {
             Console.WriteLine("Hello World!");
 
-            Console.WriteLine("Hello World! test  ");
+            Console.WriteLine("Hello World! test adf  ");
 
               
         }
